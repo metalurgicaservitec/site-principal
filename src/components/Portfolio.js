463a6df47aab065522f0eb9d8f2684ef7ae2efac
metalurgicaservitec <metalurgicaservitec@gmail.com>
2025-10-04 18:01:08 -0400

    Delete src directory

--- src/components/Portfolio.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { PortfolioCarousel } from './ui/portfolio-carousel';
-
-const Portfolio = () => {
-  return <PortfolioCarousel />;
-};
-
-export default Portfolio;
